6a1ebcf60329261adeceaa680f792fa56073ad6b
Arsen Guralnyk <a.huralhyk@lampalampa.net>
2024-02-15 13:34:13 +0200

    feature/create-user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@nestjs/common'
+import { startOfDay, subDays } from 'date-fns'
+import { hash } from 'argon2'
+
 import { PrismaService } from '../prisma.service'
 import { AuthDto } from '../auth/dto/auth.dto'
-import { hash } from 'argon2'
+import { UserDto } from './dto/user.dto'
 
 @Injectable()
 export class UserService {
@@ -26,6 +29,51 @@ export class UserService {
 		})
 	}
 
+	async getProfile(id: string) {
+		const profile = await this.getById(id)
+
+		const totalTasks = profile.tasks.length
+		const completedTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				isCompleted: true
+			}
+		})
+		const todayStart = startOfDay(new Date())
+		const weekStart = startOfDay(subDays(new Date(), 7))
+
+		const todayTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: todayStart.toISOString()
+				}
+			}
+		})
+
+		const weekTasks = await this.prisma.task.count({
+			where: {
+				userId: id,
+				createdAt: {
+					gte: weekStart.toISOString()
+				}
+			}
+		})
+
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		const { password, ...rest } = profile
+
+		return {
+			user: rest,
+			statistics: [
+				{ label: 'Total', value: totalTasks },
+				{ label: 'Completed tasks', value: completedTasks },
+				{ label: 'Today tasks', value: todayTasks },
+				{ label: 'Week tasks', value: weekTasks }
+			]
+		}
+	}
+
 	async create(dto: AuthDto) {
 		const user = {
 			email: dto.email,
@@ -35,6 +83,25 @@ export class UserService {
 
 		return this.prisma.user.create({
 			data: user
+		})
+	}
+
+	async update(id: string, dto: UserDto) {
+		let data = dto
+
+		if (dto.password) {
+			data = { ...data, password: await hash(dto.password) }
+		}
+
+		return this.prisma.user.update({
+			where: {
+				id
+			},
+			data,
+			select: {
+				name: true,
+				email: true
+			}
 		})
 	}
 }
